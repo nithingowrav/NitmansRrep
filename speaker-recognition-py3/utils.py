87198bb3ce1187ec19bc42291bd644ae235c8adc
nithingowrav <nithingowrav@gmail.com>
2019-12-14 16:30:01 +0530

    deleted duplicate

--- speaker-recognition-py3/utils.py
+++
@@ -1,8 +0,0 @@
-from scipy.io import wavfile
-
-def read_wav(fname):
-    fs, signal = wavfile.read(fname)
-    if len(signal.shape) != 1:
-        print("convert stereo to mono")
-        signal = signal[:,0]
-    return fs, signal
